46e6e1c0884f51ed092afd136cb67c9889a745ac
Bobobek Turdiyev <info@programmer.uz>
2023-08-10 02:33:40 +0500

    fix: controller

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Programmeruz\LaravelCreator\Http\Controllers\ColumnController;
 
 Route::prefix('/creator')->group( function(){
     Route::view('/', 'LaravelCreator::index')->name('index');
-    Route::post('/generate', [\App\Http\Controllers\ColumnController::class, 'generateColumns'])->name('creator.generate');
+    Route::post('/generate', [ColumnController::class, 'generateColumns'])->name('creator.generate');
 });
 
