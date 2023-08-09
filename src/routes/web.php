66ef9082ebb565d5d8c6141c9ce8755805369575
Bobobek Turdiyev <info@programmer.uz>
2023-08-10 02:24:04 +0500

    fix: views

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/creator')->group( function(){
-    Route::view('/', 'index')->name('index');
+    Route::view('/', 'LaravelCreator::index')->name('index');
     Route::post('/generate', [\App\Http\Controllers\ColumnController::class, 'generateColumns'])->name('creator.generate');
 });
 
